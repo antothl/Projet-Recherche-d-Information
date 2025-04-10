ef5c290ae07092c87620268fb73a2c5021bce28e
Antoine Théologien <123930817+antothl@users.noreply.github.com>
2025-04-10 22:47:12 +0200

    miam

--- Code_original/deep-qa/run_eval.sh
+++ Code_original/deep-qa/run_eval.sh
@@ -5,5 +5,5 @@ judgement=${exp_dir}/gold.txt
 output=${exp_dir}/submission.txt
 
 ./trec_eval-8.0/trec_eval -q -c ${judgement} ${output} > ${output}.treceval
-tail -29 ${output}.treceval | grep -e 'map' -e 'recip_rank'
+tail -29 ${output}.treceval | grep -e 'map' -e 'recip_rank' -e 'R_prec' 
 exit 0
